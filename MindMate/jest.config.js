fb1d983c3bc32038f85468bf3f99897a545d212f
BigOldMapleSyrup <nikodem.mech@hotmial.com>
2025-05-03 14:37:06 +0100

    setup tests for frontend

---
+++ MindMate/jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    preset: 'jest-expo',
+    transformIgnorePatterns: [
+      'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-navigation|expo(nent)?|@expo|expo-router|@unimodules|unimodules|@sentry/react-native|native-base|react-native-svg|@testing-library)'
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '^@/(.*)$': '<rootDir>/$1'
+    },
+    testMatch: [
+      '**/__tests__/**/*.test.[jt]s?(x)'
+    ],
+    testPathIgnorePatterns: [
+      '/node_modules/',
+      '/android/',
+      '/ios/'
+    ],
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'services/**/*.{ts,tsx}',
+      'utils/**/*.{ts,tsx}',
+      'components/**/*.{ts,tsx}',
+      '!**/node_modules/**',
+      '!**/__tests__/**'
+    ],
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    testEnvironment: 'node'
+  };
