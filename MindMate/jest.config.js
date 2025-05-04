9506ba0d94427af408fe660befcd2ea98b1b5e65
BigOldMapleSyrup <nikodem.mech@hotmial.com>
2025-05-04 21:48:19 +0100

    refactor: update dependencies in package.json and package-lock.json

--- MindMate/jest.config.js
+++ MindMate/jest.config.js
@@ -1,32 +1,26 @@
 module.exports = {
-    preset: 'jest-expo',
-    transformIgnorePatterns: [
-      'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-navigation|expo(nent)?|@expo|expo-router|@unimodules|unimodules|@sentry/react-native|native-base|react-native-svg|@testing-library)'
-    ],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-    moduleNameMapper: {
-      '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-      '^@/(.*)$': '<rootDir>/$1'
-    },
-    testMatch: [
-      '**/__tests__/**/*.test.[jt]s?(x)'
-    ],
-    testPathIgnorePatterns: [
-      '/node_modules/',
-      '/android/',
-      '/ios/'
-    ],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
-    },
-    collectCoverage: true,
-    collectCoverageFrom: [
-      'services/**/*.{ts,tsx}',
-      //'components/**/*.{ts,tsx}',
-      '!**/__tests__/**'
-    ],
-    coverageReporters: ['json', 'lcov', 'text', 'clover'],
-    testEnvironment: 'node'
-  };
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-navigation|expo(nent)?|@expo|expo-router|@unimodules|unimodules|@sentry/react-native|native-base|react-native-svg|@testing-library)",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/android/", "/ios/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "services/**/*.{ts,tsx}",
+    //'components/**/*.{ts,tsx}',
+    "!**/__tests__/**",
+  ],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  testEnvironment: "node",
+};
